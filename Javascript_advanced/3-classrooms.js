aa4db85683d6ab253813ef5fda1b75c6ea487f6d
NicoBoni55 <bonifrank55@gmail.com>
2025-01-21 15:26:42 -0300

    3-classrooms

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -2,11 +2,14 @@ function createClassRoom(numbersOfStudents) {
     if (typeof numbersOfStudents === "number") {
         function studentSeat(seat) {
             if (typeof seat === "number") {
-                return (seat);
+                return function() {
+                    return (seat);
+                }
             } else {
                 console.error("seat must be a number.")
             }
-        }    
+        }
+        let students;  
     } else {
         console.error("numbersOfStudents must be a number.");
     }
